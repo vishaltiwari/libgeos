7823b3296efbc1ec5298cb9625d457d89a54300e
vishaltiwari <hi.vishal123@gmail.com>
2013-08-22 02:19:17 +0530

    kdtree header file

---
+++ include/geos/index/kdtree/KdNode.h
@@ -0,0 +1,58 @@
+/**********************************************************************
+ *
+ * GEOS - Geometry Engine Open Source
+ * http://geos.osgeo.org
+ *
+ * Copyright (C) 2006 Refractions Research Inc.
+ *
+ * This is free software; you can redistribute and/or modify it under
+ * the terms of the GNU Lesser General Public Licence as published
+ * by the Free Software Foundation. 
+ * See the COPYING file for more information.
+ *
+ **********************************************************************
+ *
+ * Last port: index/quadtree/KdNode.java rev 1.8 (JTS-1.10)
+ *
+ **********************************************************************/
+
+#ifndef GEOS_IDX_KDTREE_KDNODE_H
+#define GEOS_IDX_KDTREE_KDNODE_H
+
+#include <geos/index/kdtree/KdNode.h>
+
+#include <geos/export.h>
+#include <geos/geom/Coordinate.h> 
+
+#include <string>
+#include <memory>
+
+namespace geos {
+	namespace geom {
+		class Coordinate;
+	}
+}
+
+namespace geos {
+namespace index { // geos::index
+namespace kdtree { // geos::index::kdtree
+
+class KdNode {
+
+private:
+	Coordinate *p;
+	void *data;
+	KdNode *left;
+	KdNode *right;
+	int count;
+
+public:
+	KdNode(const double _x,const double _y , void *data);
+
+};
+
+} // namespace geos::index::kdtree
+} // namespace geos::index
+} // namespace geos
+
+#endif // GEOS_IDX_KDTREE_KDNODE_H
