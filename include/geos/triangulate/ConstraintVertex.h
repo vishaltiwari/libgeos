102d15e6d79e2bd0a6a00520b1faf77a667cc6ab
vishaltiwari <hi.vishal123@gmail.com>
2013-08-30 02:01:04 +0530

    ConstraintVertex class added

---
+++ include/geos/triangulate/ConstraintVertex.h
@@ -0,0 +1,59 @@
+/**********************************************************************
+ *
+ * GEOS - Geometry Engine Open Source
+ * http://geos.osgeo.org
+ *
+ * Copyright (C) 2012 Excensus LLC.
+ *
+ * This is free software; you can redistribute and/or modify it under
+ * the terms of the GNU Lesser General Licence as published
+ * by the Free Software Foundation. 
+ * See the COPYING file for more information.
+ *
+ **********************************************************************
+ *
+ * Last port: triangulate/ConstraintVertex.java r524
+ *
+ **********************************************************************/
+
+#ifndef GEOS_TRIANGULATE_CONSTRAINTVERTEX_H
+#define GEOS_TRIANGULATE_CONSTRAINTVERTEX_H
+
+#include <geos/triangulate/quadedge/Vertex.h>
+namespace geos {
+namespace geom{
+	class Coordinate;
+}
+namespace triangulate { //geos.triangulate
+
+namespace quadedge {
+	class Vertex;
+}
+
+class GEOS_DLL ConstraintVertex : public geos::triangulate::quadedge::Vertex 
+{
+private:
+	bool isOnConstraint;
+	void* constraint;
+
+public:
+	ConstraintVertex(const geom::Coordinate p);
+
+	void setOnConstraint(bool isOnConstraint);
+
+	bool isConstraint();
+
+	void setConstraint(void* constraint);
+
+	void* getConstraint();
+
+protected:
+	void merge(ConstraintVertex &other);
+
+};
+
+} //namespace geos.triangulate
+} //namespace goes
+
+#endif //GEOS_TRIANGULATE_CONTRAINTVERTEX_H
+
---
+++ src/triangulate/ConstraintVertex.cpp
@@ -0,0 +1,68 @@
+/**********************************************************************
+ *
+ * GEOS - Geometry Engine Open Source
+ * http://geos.osgeo.org
+ *
+ * Copyright (C) 2012 Excensus LLC.
+ *
+ * This is free software; you can redistribute and/or modify it under
+ * the terms of the GNU Lesser General Licence as published
+ * by the Free Software Foundation. 
+ * See the COPYING file for more information.
+ *
+ **********************************************************************
+ *
+ * Last port: triangulate/ConstraintVertex.java rev. r524
+ *
+ **********************************************************************/
+
+#include <geos/triangulate/ConstraintVertex.h>
+
+#include <geos/triangulate/quadedge/Vertex.h>
+#include <geos/geom/Coordinate.h>
+
+namespace geos {
+namespace triangulate { //geos.triangulate
+
+ConstraintVertex::ConstraintVertex(const geom::Coordinate p):
+	constraint(NULL)
+{
+	Vertex::Vertex(p);
+}
+
+void
+ConstraintVertex::setOnConstraint(const bool isOnConstraint)
+{
+	this->isOnConstraint = isOnConstraint;
+}
+
+bool
+ConstraintVertex::isConstraint()
+{
+	return isOnConstraint;
+}
+
+void
+ConstraintVertex::setConstraint(void* constraint)
+{
+	isOnConstraint = 1;
+	this->constraint = constraint;
+}
+void*
+ConstraintVertex::getConstraint()
+{
+	return constraint;
+}
+
+void 
+ConstraintVertex::merge(ConstraintVertex &other)
+{
+	if(other.isOnConstraint)
+	{
+		isOnConstraint = 1;
+		constraint  = other.constraint;
+	}
+}
+
+} //namespace geos.triangulate
+} //namespace geos
