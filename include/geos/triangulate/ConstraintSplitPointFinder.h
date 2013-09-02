720b123e05204427a38cb1bb3d58417ac6d8b986
vishaltiwari <hi.vishal123@gmail.com>
2013-09-02 20:36:34 +0530

    class ../../../src/triangulate/ConstraintSplitPointFinder.cpp

---
+++ include/geos/triangulate/ConstraintSplitPointFinder.h
@@ -0,0 +1,56 @@
+/**********************************************************************
+ *
+ * GEOS - Geometry Engine Open Source
+ * http://geos.osgeo.org
+ *
+ * Copyright (C) 2012 Excensus LLC.
+ *
+ * This is free software; you can redistribute and/or modify it under
+ * the terms of the GNU Lesser General Licence as published
+ * by the Free Software Foundation. 
+ * See the COPYING file for more information.
+ *
+ **********************************************************************
+ *
+ * Last port: triangulate/quadedge/ConstraintSplitPointFinder.java r524
+ *
+ **********************************************************************/
+
+#ifndef GEOS_TRIANGULATE_CONSTRAINTSPLITPOINTFINDER_H
+#define GEOS_TRIANGULATE_CONSTRAINTSPLITPOINTFINDER_H
+
+#include <geos/triangulate/ConstraintSplitPointFinder.h>
+
+namespace geos {
+namespace geom{
+	class Coordinate;
+}
+namespace triangulate { //geos.triangulate
+
+/**
+ * An interface for strategies for determining the location of split points on constraint segments.
+ * The location of split points has a large effect on the performance and robustness of enforcing a
+ * constrained Delaunay triangulation. Poorly chosen split points can cause repeated splitting,
+ * especially at narrow constraint angles, since the split point will end up encroaching on the
+ * segment containing the original encroaching point. With detailed knowledge of the geometry of the
+ * constraints, it is sometimes possible to choose better locations for splitting.
+ * 
+ * @author mbdavis
+ */
+class GEOS_DLL ConstraintSplitPointFinder {
+public:
+	/** 
+	 * Finds a point at which to split an encroached segment to allow the original segment to appear
+	 * as edges in a constrained Delaunay triangulation.
+	 * 
+	 * @param seg the encroached segment
+	 * @param encroachPt the encroaching point
+	 * @return the point at which to split the encroached segment
+	 */
+	virtual geom::Coordinate findSplitPoint(Segment seg, Coordinate encroachPt) = 0;
+	virtual ~ConstraintSplitPointFinder() = 0 ;
+} ; 
+} //namespace geos.triangulate
+} //namespace goes
+
+#endif // GEOS_TRIANGULATE_CONSTRAINTSPLITPOINTFINDER_H
---
+++ src/triangulate/ConstraintSplitPointFinder.cpp
@@ -0,0 +1,28 @@
+/**********************************************************************
+ *
+ * GEOS - Geometry Engine Open Source
+ * http://geos.osgeo.org
+ *
+ * Copyright (C) 2012 Excensus LLC.
+ *
+ * This is free software; you can redistribute and/or modify it under
+ * the terms of the GNU Lesser General Licence as published
+ * by the Free Software Foundation. 
+ * See the COPYING file for more information.
+ *
+ **********************************************************************
+ *
+ * Last port: triangulate/quadedge/ConstraintSplitPointFinder.java r524
+ *
+ **********************************************************************/
+
+#include <geos/triangulate/ConstraintSplitPointFinder.h>
+
+namespace geos {
+namespace triangulate { //geos.triangulate
+
+ConstraintSplitPointFinder::~ConstraintSplitPointFinder() {}
+
+} //namespace geos.triangulate
+} //namespace geos
+
